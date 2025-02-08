512d69e044edc472565ff7ba5dd99cb98d8e1f2f
codingtech <codingtech390@gmail.com>
2025-02-08 12:48:14 +0530

    Navbar is now complete and secured with clerk authentication. package env has been updated. User can now see and manage their authentication related things in the control panel. All managed by Clerk.

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,14 @@ import RegisterPage from "../routes/RegisterPage.jsx";
 import PostListPage from "../routes/PostListPage.jsx";
 import SinglePostPage from "../routes/SinglePostPage.jsx";
 import MainLayout from "../layouts/MainLayout.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 const router = createBrowserRouter([
   {
@@ -51,6 +59,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>
 );
